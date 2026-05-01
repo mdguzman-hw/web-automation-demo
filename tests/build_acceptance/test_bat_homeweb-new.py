6d32496442317897104d3f09caaf49b6614d49e2
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-05-01 14:06:58 -0600

    Create new test suite for Homeweb 3.1.0

---
+++ tests/build_acceptance/test_bat_homeweb-new.py
@@ -0,0 +1,49 @@
+# Copyright © 2026 - Homewood Health Inc.
+
+################# BUILD ACCEPTANCE ################
+##################### HOMEWEB #####################
+################# NEW PORTAL - BETA ###############
+import pytest
+from selenium.webdriver.common.by import By
+
+# CORE MODULES
+# Registration
+# Login
+# Dashboard - S1
+# Dashboard - S2
+# Dashboard - S3
+# Discover - Mental Health
+# Discover - Wellness
+# Discover - Work-Life
+# Journey - Plans
+# Journey - Sessions
+# Journey - Appointments
+# Smart Care Navigation
+# Booking
+# Profile
+# Library
+# Messages
+
+# BAT-WEB-001 | Navigate Homeweb
+# BAT-WEB-002 | Homeweb Landing Articles
+# BAT-WEB-003 | Homeweb Login
+# BAT-WEB-004 | Homeweb Resource
+# BAT-WEB-005 | Sentio kickout
+# BAT-WEB-006 | Complete Pathfinder Assessment - Scenario 4
+# BAT-WEB-007 | Homeweb Logout
+# BAT-WEB-008 | Homeweb Login - Different Account
+# BAT-WEB-009 | Kickouts
+# BAT-WEB-010 | Course conesnt
+# BAT-WEB-011 | Cancel Active Services
+# BAT-WEB-012 | Live Chat
+# BAT-WEB-013 | Complete Pathfinder Assessment - Scenario 1
+# BAT-WEB-014 | Complete Pathfinder Assessment - Scenario 2
+# BAT-WEB-015 | Complete Pathfinder Assessment - Scenario 3
+# BAT-WEB-016 | Create Pathfinder Booking
+# BAT-WEB-017 | Complete Pathfinder Booking
+# BAT-WEB-018 | Complete Pathfinder Assessment - Scenario 5
+# BAT-WEB-019 | Resource Library
+# BAT-WEB-020 | Primary Category
+# BAT-WEB-021 | Sub category
+# BAT-WEB-022 | Search
+# BAT-WEB-023 | Embedded resources
